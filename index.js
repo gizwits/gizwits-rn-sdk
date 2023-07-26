702d923a65d1db44c8095b67a10ef08afde6c730
王锐彬 <492960423@qq.com>
2023-07-26 15:38:06 +0800

    js 容错

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//@ts-ignore we want to ignore everything
+// @ts-ignore we want to ignore everything
 // else in global except what we need to access.
 // Maybe there is a better way to do this.
 import { NativeModules } from 'react-native';
@@ -8,11 +8,11 @@ const { RNGizwitsRnSdk, RNGizwitsRnDevice } = NativeModules;
 // Installing JSI Bindings as done by
 // https://github.com/mrousavy/react-native-mmkv
 
-//@ts-ignore
+// @ts-ignore
 const RNGizwitsRnSdkJSI: {
   setSubscribe(did: string, mac: string, productKey: string, ps: string, subscribed: bool): void;
   getVersion(): string;
-  //@ts-ignore
+  // @ts-ignore
 } = global;
 
 export function isLoaded() {
@@ -25,8 +25,7 @@ export function isDeviceLoaded() {
 
 if (!isLoaded()) {
   const result = RNGizwitsRnSdk?.install();
-  if (!result && !isLoaded())
-    throw new Error('JSI bindings were not installed for: RNGizwitsRnSdk Module');
+  if (!result && !isLoaded()) { throw new Error('JSI bindings were not installed for: RNGizwitsRnSdk Module'); }
 
   if (!isLoaded()) {
     throw new Error('JSI bindings were not installed for: RNGizwitsRnSdk Module');
@@ -34,8 +33,7 @@ if (!isLoaded()) {
 }
 if (!isDeviceLoaded()) {
   const result = RNGizwitsRnDevice?.install();
-  if (!result && !isDeviceLoaded())
-    throw new Error('JSI bindings were not installed for: RNGizwitsRnDevice Module');
+  if (!result && !isDeviceLoaded()) { throw new Error('JSI bindings were not installed for: RNGizwitsRnDevice Module'); }
 
   if (!isDeviceLoaded()) {
     throw new Error('JSI bindings were not installed for: RNGizwitsRnDevice Module');
@@ -54,40 +52,59 @@ function debounce(func, delay) {
   };
 }
 const callbacks = {
-    "GizDeviceListNotifications": () => {},
-    "GizDeviceNetStatusNotifications": () => {},
-    "GizDeviceStatusNotifications": () => {},
-    "GizBleDeviceListNotifications": () => {},
+  GizDeviceListNotifications: () => {},
+  GizDeviceNetStatusNotifications: () => {},
+  GizDeviceStatusNotifications: () => {},
+  GizBleDeviceListNotifications: () => {},
 
-}
+};
 RNGizwitsRnSdkJSI.addListener = (name, callback) => {
-    callbacks[name] = callback
-}
+  callbacks[name] = callback;
+};
 
 global.GizDeviceListNotifications = debounce((data) => {
-   callbacks["GizDeviceListNotifications"] && callbacks["GizDeviceListNotifications"](JSON.parse(data))
-}, 600)
+  setTimeout(() => {
+    try {
+      callbacks.GizDeviceListNotifications && callbacks.GizDeviceListNotifications(JSON.parse(data));
+    } catch (error) {
+      console.error('GizDeviceListNotifications callback error', error);
+    }
+  }, 1);
+}, 600);
 
 global.GizBleDeviceListNotifications = debounce((data) => {
-   callbacks["GizBleDeviceListNotifications"] && callbacks["GizBleDeviceListNotifications"](JSON.parse(data))
-}, 600)
+  setTimeout(() => {
+    try {
+      callbacks.GizBleDeviceListNotifications && callbacks.GizBleDeviceListNotifications(JSON.parse(data));
+    } catch (error) {
+      console.error('GizBleDeviceListNotifications callback error', error);
+    }
+  }, 1);
+}, 600);
 
 global.GizDeviceStatusNotifications = (data) => {
-   callbacks["GizDeviceStatusNotifications"] && callbacks["GizDeviceStatusNotifications"](JSON.parse(data))
-}
+  setTimeout(() => {
+    try {
+      callbacks.GizDeviceStatusNotifications && callbacks.GizDeviceStatusNotifications(JSON.parse(data));
+    } catch (error) {
+      console.error('GizDeviceStatusNotifications callback error', error);
+    }
+  }, 1);
+};
 
 global.GizDeviceNetStatusNotifications = (data) => {
-  try {
-    const newData = JSON.parse(data);
-    const {mac, did,productKey} = newData.device;
-    const {netStatus} = newData;
-    callbacks["GizDeviceNetStatusNotifications"] && callbacks["GizDeviceNetStatusNotifications"](mac, did, productKey, parseInt(netStatus))
-  } catch (error) {
-    console.error("GizDeviceNetStatusNotifications parse error: ",error)
-  }
-}
-
-
+  setTimeout(() => {
+    try {
+      const newData = JSON.parse(data);
+      const { mac, did, productKey } = newData.device;
+      const { netStatus } = newData;
+      callbacks.GizDeviceNetStatusNotifications
+      && callbacks.GizDeviceNetStatusNotifications(mac, did, productKey, parseInt(netStatus, 10));
+    } catch (error) {
+      console.error('GizDeviceNetStatusNotifications parse error: ', error);
+    }
+  }, 1);
+};
 
 export default RNGizwitsRnSdk;
-export {RNGizwitsRnSdk, RNGizwitsRnDevice, RNGizwitsRnSdkJSI};
+export { RNGizwitsRnSdk, RNGizwitsRnDevice, RNGizwitsRnSdkJSI };
