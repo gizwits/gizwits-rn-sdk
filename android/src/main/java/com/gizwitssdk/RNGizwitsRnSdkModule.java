ec730dcbb45b4f0a983751050c07ef9cebfc4a82
王锐彬 <492960423@qq.com>
2024-06-08 17:11:18 +0800

    增加 airlink15

--- android/src/main/java/com/gizwitssdk/RNGizwitsRnSdkModule.java
+++ android/src/main/java/com/gizwitssdk/RNGizwitsRnSdkModule.java
@@ -935,6 +935,15 @@ public class RNGizwitsRnSdkModule extends ReactContextBaseJavaModule {
                         case 12:
                             types.add(GizWifiGAgentType.GizGAgentFlyLink);
                             break;
+                        case 13:
+                            types.add(GizWifiGAgentType.GizGAgentMxchipAWS);
+                            break;
+                        case 14:
+                            types.add(GizWifiGAgentType.GizGAgentHFV8);
+                            break;
+                        case 15:
+                            types.add(GizWifiGAgentType.GizGAgentESPBroadcast);
+                            break;
                         default:
                             types.add(GizWifiGAgentType.GizGAgentESP);
                             break;
@@ -1061,6 +1070,15 @@ public class RNGizwitsRnSdkModule extends ReactContextBaseJavaModule {
                             break;
                         case 12:
                             types.add(GizWifiGAgentType.GizGAgentFlyLink);
+                            break;
+                        case 13:
+                            types.add(GizWifiGAgentType.GizGAgentMxchipAWS);
+                            break;
+                        case 14:
+                            types.add(GizWifiGAgentType.GizGAgentHFV8);
+                            break;
+                        case 15:
+                            types.add(GizWifiGAgentType.GizGAgentESPBroadcast);
                             break;
                         default:
                             types.add(GizWifiGAgentType.GizGAgentESP);
